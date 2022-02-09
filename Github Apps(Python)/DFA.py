f6be28e5d8273fb59656db254c0ba7eb9562c74e
Deni Cabaravdic <37716587+d3h1@users.noreply.github.com>
2022-02-08 19:13:07 -0800

    code

---
+++ Github Apps(Python)/DFA.py
@@ -0,0 +1,46 @@
+NUM_STATES = 6
+
+states = ['q' + str(i) for i in range(NUM_STATES)]
+transitionFunction = {
+    0: {
+        'a': 1,
+        'b': 5
+    },
+    1: {
+        'a': 5,
+        'b': 2
+    },
+    2: {
+        'a': 5,
+        'b': 3
+    },
+    3: {
+        'a': 4,
+        'b': 5
+    },
+    4: {
+        'a': 4,
+        'b': 4
+    },
+    5: {
+        'a': 5,
+        'b': 5
+    }
+}
+initialState = 0
+finalStates = [4]
+
+currentState = initialState
+
+print("Initial State: " + states[currentState])
+
+str = input("Enter a string to test: ")
+
+path = []
+
+for letter in str:
+    currentState = transitionFunction[currentState][letter]
+    path.append(states[currentState])
+print("->".join(path))
+
+print(currentState in finalStates)
